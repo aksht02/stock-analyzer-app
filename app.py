2abba683e45bec3e413e41072148da8edc98199c
aksht02 <aksht02@gmail.com>
2026-02-15 12:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,14 +66,14 @@ if st.button("Fetch Stock Data"):
 
     else:
         st.warning("Please enter a stock symbol")
-        st.markdown(
+# ---------------- FOOTER ----------------
+st.markdown(
     """
     <hr>
     <div style="text-align: center; font-size:14px; opacity:0.6;">
-        Created by <b>Akshay Tak</b> 🚀 | 
-        <a href="https://github.com/aksht02" target="_blank">GitHub</a>
+        Created by <b>Akshay Tak</b> 🚀 |
+        <a href="https://github.com/aksht02" target="_blank"> GitHub</a>
     </div>
     """,
     unsafe_allow_html=True
 )
-
